65c41d99e0836556b23796d69d9ac2f208d90fff
Poutrignou <thibaut.boguszewski@gmail.com>
2022-09-13 22:34:55 +0200

    13/09/2022

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
-{
+class HomeController extends AbstractController{
+
     #[Route('/', 'home.index', methods:['GET'])]
     public function index(): Response
     {
--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,18 +3,34 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
-    // On importe le repository dans la fonction index();
-    public function index(IngredientRepository $repository): Response
-    {
-        $ingredient = $repository->findAll();
 
+
+    /**
+     *  This function display all ingredients
+     * 
+     * @param IngredientRepository $repossitory
+     * @param PaginatorIterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+
+    #[Route('/ingredient', name: 'app_ingredient', methods:['GET'])]
+    // On importe le repository dans la fonction index();
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $ingredient = $paginator->paginate(
+            $ingredient = $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10 
+        );
         return $this->render('pages/ingredient/index.html.twig',[
             'ingredients' => $ingredient
         ]);
